39d8fa2e78252d0b4891c5c08d4f0a8d2e7a6c3d
PascalH214 <pascal02012004@freenet.de>
2026-03-30 18:17:05 +0000

    Add vsCode-settings

--- home/vsCode/default.nix
+++ home/vsCode/default.nix
@@ -1,10 +1,21 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      jnoortheen.nix-ide
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        jnoortheen.nix-ide
+        catppuccin.catppuccin-vsc
+      ];
+      userSettings = {
+        "editor" = {
+          "lineNumbers" = "relative";
+        };
+        "workbench" = {
+          "colorTheme" = "Catppuccin Mocha"; 
+        };
+      };
+    };
   };
 }
