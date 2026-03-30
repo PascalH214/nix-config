31a4b87134f98ece8758cfd9222e1eb69e4e0546
PascalH214 <pascal02012004@freenet.de>
2026-03-30 13:00:59 +0000

    Refactore btop

--- home/btop/default.nix
+++ home/btop/default.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }: {
-  xdg.configFile."btop" = {
-    source = ./config;
-    recursive = true;
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "catppuccin_mocha";
+    };
+    themes = {
+      "catppuccin_mocha" = builtins.readFile ./files/catppuccin_mocha.theme;
+    };
   };
-
-  home.packages = [ pkgs.btop ];
 }
