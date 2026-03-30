98248c9a75ded86224fdc9a97264582d82cb2888
PascalH214 <pascal02012004@freenet.de>
2026-03-30 18:41:01 +0000

    Add amdgpu_top and kernel param for amd

--- modules/gpu/amd/default.nix
+++ modules/gpu/amd/default.nix
@@ -1,8 +1,17 @@
 {
+  pkgs,
   ...
 }: {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    amdgpu_top
+  ];
+
+  boot.kernelParams = [
+    "amdgpu.ppfeaturemask=0xffffffff"
+  ];
 }
