03f05ac2cd4c69e438773105a3828e3ef64284cb
PascalH214 <pascal02012004@freenet.de>
2026-04-02 12:31:38 +0000

    Add default-homepage to chromium

--- home/chrome/default.nix
+++ home/chrome/default.nix
@@ -7,5 +7,15 @@
       "nngceckbapebfimnlniiiahkandclblb" # bitwarden
       "gighmmpiobklfepjocnamgkkbiglidom" # AdBlock
     ];
+
+    dictionaries = [
+      pkgs.hunspellDictsChromium.en_US
+      pkgs.hunspellDictsChromium.de_DE
+    ];
+
+    commandLineArgs = [
+      "https://glance.pascal-lab.com/"
+      "--new-window"
+    ];
   };
 }
