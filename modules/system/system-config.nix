84f7e65978d737b9e7c5b064359ec6e6bace2241
PascalH214 <pascal02012004@freenet.de>
2026-04-02 19:14:41 +0000

    Use iwd as backend for NetworkManager

--- modules/system/system-config.nix
+++ modules/system/system-config.nix
@@ -1,5 +1,19 @@
 { pkgs, lib, ... }: {
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+    wireless.enable = false; # disables wpa_supplicant
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        Settings = {
+          AutoConnect = true;
+        };
+      };
+    };
+  };
 
   time.timeZone = "Europe/Berlin";
 
